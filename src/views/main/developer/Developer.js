6e9de85f5f4ba5dae22d36d520f310a387ddcc54
prasad-gawade-xircls <prasad.gawade@xircls.com>
2023-10-23 14:03:07 +0530

    section 1 changes

--- src/views/main/developer/Developer.js
+++ src/views/main/developer/Developer.js
@@ -1,31 +1,6 @@
-// import React from 'react'
-// import './style.scss'
-// import { Section1 } from './componets/section1/Section1'
-// import { Section2 } from './componets/section2/Section2'
-// import { Section3 } from './componets/section3/Section3'
-// import { Section4 } from './componets/section4/Section4'
-// import { Section5 } from './componets/section5/Section5'
-// import { Section6 } from './componets/section6/Section6'
-// import { Section7 } from './componets/section7/Section7'
-// // import { Section8 } from './componets/section8/Section8'
-
-// export const Developer = () => {
-//   return (
-//     <div className='home'>
-//         <Section1/>
-//         <Section2/>
-//         <Section3/>
-//         <Section4/>
-//         <Section5/>
-//         <Section6/>
-//         <Section7/>
-//         {/* <Section8/> */}
-//     </div>
-//   )
-// }
-
 
 import React from 'react';
+import Navbar from '../navbar/Navbar'
 import { Section1 } from './componets/section1/Section1';
 import { Section2 } from './componets/section2/Section2';
 import { Section3 } from './componets/section3/Section3';
@@ -33,6 +8,7 @@ import { Section4 } from './componets/section4/Section4';
 import { Section5 } from './componets/section5/Section5';
 import { Section6 } from './componets/section6/Section6';
 import { Section7 } from './componets/section7/Section7';
+import Footer from '../footer/Footer'
 
 export const Developer = () => {
   const inlineStyles = {
@@ -45,6 +21,7 @@ export const Developer = () => {
 
   return (
     <div style={inlineStyles} className='home'>
+      <Navbar/>
       <Section1 style={fontStyles} />
       <Section2 style={fontStyles} />
       <Section3 style={fontStyles} />
@@ -52,6 +29,7 @@ export const Developer = () => {
       <Section5 style={fontStyles} />
       <Section6 style={fontStyles} />
       <Section7 style={fontStyles} />
+      <Footer />
     </div>
   );
 };
--- src/views/main/developer/componets/section1/Section1.js
+++ src/views/main/developer/componets/section1/Section1.js
@@ -1,120 +1,32 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Col, Row } from 'reactstrap';
+import { BiRightArrowAlt } from 'react-icons/bi'
+import '../../style.scss'
+import Button from 'react-bootstrap/Button';
 
 export const Section1 = () => {
-    const rowStyles = {
-        marginTop: "170px",
-    };
-
-    const sectionStyles = {
-        background: "#e5e7eb",
-        borderRadius: "16px",
-        boxShadow: "0 0 15px 5px rgba(0, 0, 0, 0.1)",
-    };
-
-    const headerStyles = {
-        fontWeight: "700",
-    };
-
-    const buttonStyles = {
-        marginTop: "20px",
-    };
-
-    const useCasesButtonStyles = {
-        borderRadius: 'none',
-        marginTop: '10px',
-    };
-
-    const olStyles = {
-        counterReset: 'li',
-        padding: 10,
-        textShadow: '0 1px 0 rgba(255,255,255,.5)',
-    };
-
-    const pStyles = {
-        position: 'relative',
-        display: 'block',
-        padding: '.6em 3em .6em 3em',
-        margin: '.5em 0',
-        color: '#444',
-        textDecoration: 'none',
-        borderRadius: '.3em',
-        transition: '0.3s ease-out',
-    };
-
-    const pBeforeStyles = {
-        content: 'counter(li)',
-        counterIncrement: 'li',
-        position: 'absolute',
-        left: '-1em',
-        top: '50%',
-        marginTop: '-1em',
-        height: '2.5em',
-        width: '2.3em',
-        lineHeight: '2em',
-        textAlign: 'center',
-        fontWeight: 'bold',
-        borderRadius: '2em',
-        transition: 'all 0.3s ease-out',
-    };
-
     return (
-        <Row style={rowStyles} className="justify-content-center">
-            <Col lg="10" xs="10">
-                <Row style={sectionStyles} className="sh p-5">
-                    <Col>
+        <div className='partnerPage pt-5'>
+            <Row className='justify-content-center'>
+                <Col sm="11" xs="11" lg="10" md="11">
+                    <Card className='cookieCard p-5 border' style={{ background: "#e5e7eb", border: "1px solid #dcdddf" }}>
                         <div>
-                            <h1 style={headerStyles} className="display-3 fw-bolder m-0 mt-2">Become a Technology Partner</h1>
-                        </div>
-                        <div>
-                            <h3 className="fs-4 m-0 mt-2">Help your clients automate and upgrade their marketing. <br /> Solve critical pain points and offer innovative solutions to expand your business.</h3>
-                        </div>
-                        <div className="d-flex mt-3 flex-wrap gap-2">
-                            <div>
-                                <Button className="btn-lg fs-3 px-3 py-1" variant="success" style={buttonStyles}>
-                                    Start Building
-                                </Button>
-                            </div>
-                            <div>
-                                <Button className="btn-lg fs-3 px-3 py-1" variant="outline-success" style={buttonStyles}>Explore APIs</Button>
+                            <h1 className='mainHeader display-3 main-heading fw-bolder m-0 mt-2'>
+                                Become a Technology Partner
+                            </h1>
+                            <h3 className='text-black m-0 fs-3'>
+                                Help your clients automate and upgrade their marketing.<br /> Solve critical pain points and offer innovative solutions to expand your business
+                            </h3>
+                            <br />
+                            <div className='d-flex gap-2 '>
+                                <Button variant="success my-1 text-white px-3 py-1">Start Building</Button>
+                                <Button variant="outline-success my-1 px-3 py-1">Explore APIs</Button>
                             </div>
                         </div>
-                    </Col>
-                </Row>
-                <Row style={rowStyles}>
-                    <Col className="d-flex justify-content-md-start" md={6} sm={12}>
-                        <div className="d-flex flex-column align-items-center">
-                            <h2 className="display-5 fw-bold text-md-start text-center">Discover the next generation of marketing-centric open APIs.</h2>
-                            <Button className="btn-lg fs-3 px-3 py-1 mt-5" variant="dark" style={useCasesButtonStyles}>See Use Cases</Button>
-                        </div>
-                    </Col>
-                    <Col md={6} sm={12} className="d-flex justify-content-md-end justify-content-center">
-                        <ol style={olStyles}>
-                            <li className=" fs-4 ">
-                                <p style={pStyles}>Cross-Marketing</p>
-                            </li>
-                            <li className="fs-4">
-                                <p style={pStyles}>Rewards & Incentives</p>
-                            </li>
-                            <li className="fs-4">
-                                <p style={pStyles}>CRM</p>
-                            </li>
-                            <li className="fs-4">
-                                <p style={pStyles}>Buyer Journey Optimization</p>
-                            </li>
-                            <li className="fs-4">
-                                <p style={pStyles}>Data Collection</p>
-                            </li>
-                            <li className="fs-4">
-                                <p style={pStyles}>Social Tools</p>
-                            </li>
-                        </ol>
-                    </Col>
-                </Row>
-            </Col>
-        </Row>
+                    </Card>
+                </Col>
+            </Row>
+        </div>
     );
-};
+}
